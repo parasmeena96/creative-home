8944457d40e8b47407a9a5561106dde26d01fec3
parasmeena96 <parasmeena96@gmail.com>
2019-05-28 11:48:20 +0530

    add

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,27 @@
- 
- 
+
+$(document).ready(function () {
+    $('#seacrh_').click(function () {
+        $('#searchPart').toggle().css({
+            "display": "block",
+            "margin-top": "2%"
+        });
+    });
+});
+
+// hero slider 
+
+wow = new WOW(
+    {
+        animateClass: 'animated',
+        offset: 100,
+        callback: function (box) {
+            console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+    }
+);
+wow.init();
+document.getElementById('moar').onclick = function () {
+    var section = document.createElement('section');
+    section.className = 'section--purple wow fadeInDown';
+    this.parentNode.insertBefore(section, this);
+};
